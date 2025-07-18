6355301522ea69d31c3532ac20055d14b8366bf1
Suilam <Suilamng@gmail.com>
2025-07-18 10:55:11 -0400

    autoplay

--- src/components/Tarjetaplay.tsx
+++ src/components/Tarjetaplay.tsx
@@ -1,41 +1,36 @@
-import React from 'react'
-import type { Song } from '../types/songs'
-import { $currentSong } from '../store';
+import React from 'react';
 import { MdOutlinePlayCircleFilled } from 'react-icons/md';
 import { MdReplay } from 'react-icons/md';
 import { MdSkipNext } from 'react-icons/md';
 import { MdOutlinePlaylistAdd } from 'react-icons/md';
+import { $currentSong } from '../store';
+import type { Song } from '../types/songs';
 
+export default function () {
 
-interface Props {
+    const [song, setSong] = React.useState<null | Song>(null)
 
-    song: Song
-}
+    React.useEffect(function() {
+        $currentSong.subscribe(function(state) {
 
+            setSong(state)
+        })
+    }, [] )
 
-export default function ({ song }: Props) {
-
-    function handlerClick() {
-
-        console.log('Click en la cancion' + song.title);
-
-        $currentSong.set(song);
-
-
-
-    }
 
     return (
-
         <div className="flex flex-col items-center text-center gap-1">
             <div>
-                <img src={song.image.url} alt="" className="w-full h-64 object-cover rounded-2xl" />
+                <img src={song?.image.url} alt="" className="w-full h-64 object-cover rounded-4xl" />
             </div>
 
             <div>
-                <h1 className="font-bold text-xl">{song.title}</h1>
-                <p>{song.author}</p>
+                <h1 className="font-bold text-xl">{song?.title}</h1>
+                <p>{song?.author}</p>
             </div>
+
+            <audio src={song?.audio.url} controls autoPlay></audio>
+            
 
             <div className="flex flex-row gap-1">
                 <span>0.00</span>
@@ -47,18 +42,19 @@ export default function ({ song }: Props) {
 
                 < MdReplay className="text-4xl text-violet-500 hover:text-violet-700 transition-all duration-150 ease-in" />
                 <MdSkipNext className="text-6xl text-violet-500 hover:text-violet-700 transition-all duration-150 ease-in -scale-100" />
-                <MdOutlinePlayCircleFilled onClick={handlerClick} className="text-6xl text-violet-500 hover:text-violet-700 transition-all duration-150 ease-in" />
+                <MdOutlinePlayCircleFilled className="text-6xl text-violet-500 hover:text-violet-700 transition-all duration-150 ease-in" />
                 <MdSkipNext className="text-6xl text-violet-500 hover:text-violet-700 transition-all duration-150 ease-in" />
                 <MdOutlinePlaylistAdd className="text-4xl text-violet-500 hover:text-violet-700 transition-all duration-150 ease-in" />
 
             </div>
         </div>
 
-
-
-
-
     )
-
 }
+
+
+
+
+
+
 
